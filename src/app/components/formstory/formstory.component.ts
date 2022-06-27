1810e102758ca56af78b64bee09c99093ffaf05c
bhupendrasinghacme <bhupendrasingh@acmeintech.in>
2022-06-27 18:02:45 +0530

    update 27/06/022

--- src/app/components/formstory/formstory.component.ts
+++ src/app/components/formstory/formstory.component.ts
@@ -13,7 +13,7 @@ export class FormstoryComponent implements OnInit {
   ngOnInit() {
     this.ionicForm = this.formBuilder.group({
       name: ['', [Validators.required]],
-      email: ['', [Validators.required]],
+      email: ['', [Validators.required,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
       school: [''],
       country: [''],
       publish_name: [''],
