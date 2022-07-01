4a8d57e6c3039243535382a2bbedd068e84f8072
bhupendrasinghacme <bhupendrasingh@acmeintech.in>
2022-07-01 13:27:45 +0530

    save development 1/7/2022

--- src/app/chatroom/chatroom.page.ts
+++ src/app/chatroom/chatroom.page.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
 import { ToastController } from '@ionic/angular';
 import { Observable } from 'rxjs';
+import { IonContent } from '@ionic/angular';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { AuthenticationService } from './../services/authentication.service';
-
 @Component({
   selector: 'app-chatroom',
   templateUrl: './chatroom.page.html',
@@ -11,9 +11,11 @@ import { AuthenticationService } from './../services/authentication.service';
 })
 
 export class ChatroomPage  implements OnInit {
+  @ViewChild(IonContent) content: IonContent;
   message = '';
   currentUser = 'bhupendra';
   messages:any;
+selected = [];
   private itemDoc: AngularFirestoreCollection<any>;
   item:Observable<any[]>;
   user_Data:any;
@@ -25,7 +27,7 @@ export class ChatroomPage  implements OnInit {
   }
 
   ngOnInit() {
-    this.itemDoc = this.db.collection<any>('messages');
+    this.itemDoc = this.db.collection<any>('messages', ref => ref.orderBy('createdAt'));
     this.authService.getUserData().then(item => {
       this.user_Data = JSON.parse(item.value);
       this.currentUser = this.user_Data.displayName;
@@ -35,10 +37,13 @@ export class ChatroomPage  implements OnInit {
 
   }
   sendMessage() {
-    let data = {user:this.currentUser,msg:this.message,createdAt:'29/06/2022'};
+    let myDate = new Date();
+    let myDateTemp = new Date(myDate);
+    let data = {user:this.currentUser,msg:this.message,createdAt:myDateTemp};
     this.itemDoc.add(data).then(itm=>{
-      this.getMessage();
+      // this.getMessage();
       this.message = '';
+      this.content.scrollToBottom();
     })
     
   }
@@ -48,4 +53,24 @@ getMessage(){
     this.messages = item;
   })
 }
+
+// getChatMessages() {
+//   let users = [];
+//   return this.getUsers().pipe(
+//     switchMap(res => {
+//       users = res;
+//       return this.afs.collection('messages', ref => ref.orderBy('createdAt')).valueChanges({ idField: 'id' }) as Observable<Message[]>;
+//     }),
+//     map(messages => {
+//       // Get the real name for each user
+//       for (let m of messages) {          
+//         m.fromName = this.getUserForMsg(m.from, users);
+//         m.myMsg = this.currentUser.uid === m.from;
+//       }        
+//       return messages
+//     })
+//   )
+// }
+
+
 }
