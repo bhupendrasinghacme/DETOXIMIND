388b98417aa23bdceae2535df9f1e952d1f9134b
bhupendrasinghacme <bhupendrasingh@acmeintech.in>
2022-07-01 18:54:52 +0530

    save development 1/7/2022

--- src/app/signuppage/signuppage.page.ts
+++ src/app/signuppage/signuppage.page.ts
@@ -13,9 +13,9 @@ import { environment } from 'src/environments/environment';
 export class SignuppagePage implements OnInit {
   credentials: FormGroup;
   httpOptions = {
-    headers: new HttpHeaders({ 
+    headers: new HttpHeaders({
       'Content-Type': 'application/json',
-      'Authorization' : 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvd3d3LmRldG94aW1pbmQuY29tIiwiaWF0IjoxNjU2NTk1ODAxLCJuYmYiOjE2NTY1OTU4MDEsImV4cCI6MTY1NzIwMDYwMSwiZGF0YSI6eyJ1c2VyIjp7ImlkIjo0LCJkZXZpY2UiOiIiLCJwYXNzIjoiZTE4ZTA4NDE4MjVkYjkwNmJjMDEzMmM5YzBlZDEzMWQifX19.-uQAiXFTPGDrCeeajKhZB52kPR3RyM9J5a6qVBfbkhI' 
+      'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvd3d3LmRldG94aW1pbmQuY29tIiwiaWF0IjoxNjU2NTk1ODAxLCJuYmYiOjE2NTY1OTU4MDEsImV4cCI6MTY1NzIwMDYwMSwiZGF0YSI6eyJ1c2VyIjp7ImlkIjo0LCJkZXZpY2UiOiIiLCJwYXNzIjoiZTE4ZTA4NDE4MjVkYjkwNmJjMDEzMmM5YzBlZDEzMWQifX19.-uQAiXFTPGDrCeeajKhZB52kPR3RyM9J5a6qVBfbkhI'
     })
   };
   constructor(
@@ -34,20 +34,20 @@ export class SignuppagePage implements OnInit {
     });
   }
 
-  async signup(){
+  async signup() {
     const loading = await this.loadingController.create({
       cssClass: 'my-custom-class',
       message: 'Please wait...',
-      spinner:'lines-sharp'
+      spinner: 'lines-sharp'
     });
     await loading.present();
-  this.httpClient.post(environment.wordpress.api_url+"users", JSON.stringify(this.credentials.value), this.httpOptions).subscribe(async item=>{
-    //  console.log(item);
-    await loading.dismiss();
-          this.presentDataAlert("User Resistered Successfully.");
-      })     
+    this.httpClient.post(environment.wordpress.api_url + "users", JSON.stringify(this.credentials.value), this.httpOptions).subscribe(async item => {
+      //  console.log(item);
+      await loading.dismiss();
+      this.presentDataAlert("User Resistered Successfully.");
+    })
   }
-  async presentDataAlert(errorMessage){
+  async presentDataAlert(errorMessage) {
     const alert = await this.alertController.create({
       header: 'Sign Up ',
       message: errorMessage,
@@ -59,7 +59,7 @@ export class SignuppagePage implements OnInit {
   get email() {
     return this.credentials.get('email');
   }
-  
+
   get username() {
     return this.credentials.get('username');
   }
