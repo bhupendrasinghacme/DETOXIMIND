a1bfdee3443fb009d052fd56d7dbc4bfb0c24897
bhupendrasinghacme <bhupendrasingh@acmeintech.in>
2022-07-08 12:50:43 +0530

    design update

--- src/app/whatsisyourstory/whatsisyourstory.module.ts
+++ src/app/whatsisyourstory/whatsisyourstory.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { WhatsisyourstoryPageRoutingModule } from './whatsisyourstory-routing.module';
-
 import { WhatsisyourstoryPage } from './whatsisyourstory.page';
 import { FormstoryComponent } from '../components/formstory/formstory.component';
 
